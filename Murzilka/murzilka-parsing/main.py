961fe57d77e8bf7de8d40c75ce5511a5ef1f49e2
Peter Link <ac.comm.peterlink@gmail.com>
2014-12-14 18:40:51 +0300

    parsing comments counts
    Signed-off-by: Peter Link <ac.comm.peterlink@gmail.com>

--- Murzilka/murzilka-parsing/main.py
+++ Murzilka/murzilka-parsing/main.py
@@ -1,4 +1,4 @@
-import re, pdb
+import os, re, pdb
 
 try:
     from bs4 import BeautifulSoup
@@ -7,7 +7,8 @@ except ImportError:
 
 from tornado.httpclient import HTTPClient, HTTPRequest, HTTPResponse, HTTPError
 
-user_name = "ntv"
+data_path = "data"
+user_name = "tema"
 
 journal_pattern = "http://m.livejournal.com/read/user/{}"
 journal_profile_pattern = "http://{}.livejournal.com/profile"
@@ -15,6 +16,8 @@ journal_profile_pattern = "http://{}.livejournal.com/profile"
 journal_entry_link_beginning = "http:\/\/\m.livejournal.com\/read\/user/{}\/".format(user_name) + "\d{1,}"
 journal_entry_link_to_format = "http://m.livejournal.com/read/user/{}/".format(user_name)
 journal_entry_pattern = re.compile(journal_entry_link_beginning)
+
+entry_comments_suffix = "/comments"
 
 previous_post_link = "<a href=\"http:\/\/m.livejournal.com\/read\/user\/{}\/".format(user_name) + "\d{1,}\">Previous post<\/a>"
 previous_post_link_pattern = re.compile(previous_post_link)
@@ -30,9 +33,12 @@ http_client = HTTPClient()
 
 posts = 0
 total_posts_from_profile = 0
-posts_links_file = open("{}".format(user_name),"a+")
+posts_links_file = ""
 
 def process_profile(username):
+    global posts_links_file, data_path
+    posts_links_file = open(data_path + "/{}".format(user_name),"a+")
+
     profile_request = HTTPRequest(journal_profile_pattern.format(username), method = "GET")
     try:
         journal_profile_page = http_client.fetch(profile_request)
@@ -40,13 +46,12 @@ def process_profile(username):
 
         global total_posts_from_profile
 
-        for span_tag in page_to_parse.findAll("span"):
+        for span_tag in page_to_parse.findAll("span", {"class" : "b-account-level"}):
             if re.search(journal_created_pattern, str(span_tag)):
                 print re.search(journal_created_pattern, str(span_tag)).group(0)
                 break
 
-        for div_tag in page_to_parse.findAll("div"):
-    #        print str(link_tag)
+        for div_tag in page_to_parse.findAll("div", {"class" : "b-profile-stat-value"}):
             if str(div_tag).startswith(journal_posts_total_beginning):
                 total_count = str(div_tag).replace(",","")
                 total_posts_from_profile = int(re.search(journal_posts_total_pattern, total_count).group(0))
@@ -63,13 +68,13 @@ def check_all_possible_post_pages(post_link):
     fail = True
     print "frozen post detected: " + str(post_number) + " it's a final countdown!"
 
-    while fail:
+    while fail or post_number == 0:
         try:
             post_request = HTTPRequest(journal_entry_link_to_format + str(post_number), method = "GET")
             post_page = http_client.fetch(post_request)
 
+            post_number = post_number - 1
             if str(non_existing_entry) in post_page.body:
-                post_number = post_number - 1
                 print post_number
             else:
                 fail = False
@@ -88,11 +93,13 @@ def get_previous_post_link(post_link):
 
         global posts, posts_links_file
         result = ""
-        for link_tag in page_to_parse.findAll("a"):
+        for link_tag in page_to_parse.findAll("td", {"class":"paging-prev"}):
             if re.search(previous_post_link, str(link_tag)):
                 result = re.search(journal_entry_pattern, str(link_tag)).group(0)
                 posts = posts + 1
                 print posts
+            else:
+                return ""
     except HTTPError as message:
         print message
         print "previous post link retry"
@@ -119,7 +126,7 @@ def get_all_user_posts_links(username):
             global total_posts_from_profile
             links_for_entries = []
             link_for_first_post = ""
-            for link_tag in page_to_parse.findAll("a"):
+            for link_tag in page_to_parse.findAll("h3", {"class":"item-header"}):
                 if re.search(journal_entry_pattern, str(link_tag)):
                     link_for_first_post = re.search(journal_entry_pattern, str(link_tag)).group(0)
                     links_for_entries.append(link_for_first_post)
@@ -136,24 +143,38 @@ def get_all_user_posts_links(username):
                 current_post_link = new_link
             else:
                 if posts < total_posts_from_profile:
-                    # its frozen post
                     current_post_link = check_all_possible_post_pages(current_post_link);
+                    new_post_number = int(re.search("\d{1,}", str(current_post_link)).group(0))
+                    last_post_number = int(re.search("\d{1,}", str(links_for_entries[-1])).group(0))
+                    if new_post_number + 1 == last_post_number:
+                        print "all posts parsed"
+                        break
 
     except HTTPError as message:
         print message
 
-    print links_for_entries
-    return []#links_for_entries
+    return links_for_entries
 
-def process_post_commentators(link):
-    n_pages = get_count_of_comment_pages(link)
-    for i in range(0,n_pages):
-        parse_all_comments(link, i)
+def get_comments_count(parsed_page):
+    for tag in parsed_page.findAll("h2", {"id":"comments", "class":"p-head"}):
+        return int(re.search("\d{2,}", str(tag)).group(0))
+
+def process_post_commentators(entry_link):
+    entry_link = entry_link.rstrip()
+    get_request = HTTPRequest(entry_link + entry_comments_suffix, method = "GET")
+    comments_page = http_client.fetch(get_request)
+    page_to_parse = BeautifulSoup(comments_page.body)
+
+    comments_count = get_comments_count(page_to_parse)
+    print entry_link, comments_count
 
 def process_one_user(username):
     all_user_posts_links = get_all_user_posts_links(username)
     for link in all_user_posts_links:
         process_post_commentators(link)
+
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
 
 if process_profile(user_name):
     process_one_user(user_name)
